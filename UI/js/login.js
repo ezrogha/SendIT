3cdc2659d1f402259a49f65def64b7c1914ef1f2
Roghashin <rtimbiti@gmail.com>
2018-11-01 15:16:36 +0300

    Add orders page

---
+++ UI/js/login.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".message a").click(function() {
+    $("form").animate(
+      {
+        height: "toggle",
+        opacity: "toggle"
+      },
+      "slow"
+    );
+  });
+});
